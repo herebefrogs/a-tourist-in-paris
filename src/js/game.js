c63e7e44117f563522a5f3639a7d85a12a1beb9e
Jerome Lecomte <jerome@herebefrogs.com>
2017-08-22 21:38:54 -0400

    move player around with keyboard arrows/WASD/ZQSD

--- src/js/game.js
+++ src/js/game.js
@@ -16,6 +16,16 @@ let lastTime;
 let requestId;
 let running = true;
 
+const INDEX_X = 0
+const INDEX_Y = 1;
+const INDEX_MOVEUP = 2;
+const INDEX_MOVEDOWN = 3;
+const INDEX_MOVELEFT = 4
+const INDEX_MOVERIGHT = 5;
+const INDEX_SPEED = 6;
+const PLAYER_SPEED = 100; // px/s
+let player = [0, 0, 0, 0, 0, 0, PLAYER_SPEED];
+
 function loop() {
   if (running) {
     requestId = requestAnimationFrame(loop);
@@ -48,17 +58,30 @@ function blit() {
 function render() {
   switch (screen) {
     case GAME_SCREEN:
-      BUFFER_CTX.fillStyle = 'green';
+      BUFFER_CTX.fillStyle = 'white';
       BUFFER_CTX.fillRect(0, 0, WIDTH, HEIGHT);
+      renderEntity(player);
       break;
   }
 
   blit();
 };
 
-function update() {
+function renderEntity(entity) {
+  BUFFER_CTX.fillStyle = 'red';
+  BUFFER_CTX.fillRect(Math.round(entity[INDEX_X]), Math.round(entity[INDEX_Y]), 44, 44);
+}
+
+function setPlayerPosition(elapsedTime) {
+  const distance = elapsedTime * player[INDEX_SPEED];
+  player[INDEX_X] += (player[INDEX_MOVELEFT] + player[INDEX_MOVERIGHT]) * distance;
+  player[INDEX_Y] += (player[INDEX_MOVEUP] + player[INDEX_MOVEDOWN]) * distance;
+};
+
+function update(elapsedTime) {
   switch (screen) {
     case GAME_SCREEN:
+      setPlayerPosition(elapsedTime);
       break;
   }
 };
@@ -94,17 +117,59 @@ onresize = onrotate = function() {
 
 // input handlers
 onkeydown = function(e) {
+  // prevent itch.io from scrolling the page up/down
+  e.preventDefault();
   switch (screen) {
     case GAME_SCREEN:
-      console.log('key down', e.which);
+      switch (e.which) {
+        case 37: // Left arrow
+        case 65: // A - QWERTY
+        case 81: // Q - AZERTY
+          player[INDEX_MOVELEFT] = -1;
+          break;
+        case 38: // Up arrow
+        case 90: // W - QWERTY
+        case 87: // Z - AZERTY
+          player[INDEX_MOVEUP] = -1;
+          break;
+        case 39: // Right arrow
+        case 68: // D
+          player[INDEX_MOVERIGHT] = 1;
+          break;
+        case 40: // Down arrow
+        case 83: // S
+          player[INDEX_MOVEDOWN] = 1;
+          break;
+      }
       break;
   }
 };
 
 onkeyup = function(e) {
+  // prevent itch.io from scrolling the page up/down
+  e.preventDefault();
   switch (screen) {
     case GAME_SCREEN:
-      console.log('key up', e.which);
+      switch (e.which) {
+        case 37: // Left arrow
+        case 65: // A - QWERTY
+        case 81: // Q - AZERTY
+          player[INDEX_MOVELEFT] = 0;
+          break;
+        case 38: // Up arrow
+        case 90: // W - QWERTY
+        case 87: // Z - AZERTY
+          player[INDEX_MOVEUP] = 0;
+          break;
+        case 39: // Right arrow
+        case 68: // D
+          player[INDEX_MOVERIGHT] = 0;
+          break;
+        case 40: // Down arrow
+        case 83: // S
+          player[INDEX_MOVEDOWN] = 0;
+          break;
+      }
       break;
   }
 };
